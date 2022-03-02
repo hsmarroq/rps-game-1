33b19181673dcefefbef274a53dd6fac874cd1e6
Herson Marroquin <hsm75@Hersons-MacBook-Pro.local>
2022-03-02 00:42:16 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// 'use strict';
+// // Variables
+// let humanScore = 0;
+// let computerScore = 0;
+
+// console.log();
+
+// // Scores Text Displayed
+// document.querySelector(
+//   '#human-score'
+// ).textContent = `Human Score: ${humanScore}`;
+
+// document.querySelector(
+//   '#computer-score'
+// ).textContent = `Computer Score: ${computerScore}`;
+
+// // Adding Scores
+// const getScore = () => {};
+
+// document.querySelector('#rock').addEventListener('click', function() {
+
+// })
